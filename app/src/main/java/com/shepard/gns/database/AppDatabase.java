ddd0f2defd659c46d299373cfc2b6990c8098473
AlexMisha <michaelalex600@gmail.com>
2018-01-04 20:10:12 +0300

    task1: entities mentioned in AppDatabase

--- app/src/main/java/com/shepard/gns/database/AppDatabase.java
+++ app/src/main/java/com/shepard/gns/database/AppDatabase.java
@@ -6,6 +6,9 @@ import android.arch.persistence.room.TypeConverters;
 
 import com.shepard.gns.database.dao.TokenRepository;
 import com.shepard.gns.database.dao.UserRepository;
+import com.shepard.gns.database.entity.Branch;
+import com.shepard.gns.database.entity.Commit;
+import com.shepard.gns.database.entity.Project;
 import com.shepard.gns.database.entity.Token;
 import com.shepard.gns.database.entity.User;
 
@@ -13,7 +16,8 @@ import com.shepard.gns.database.entity.User;
  * @author shepard
  * @since 24.12.2017
  */
-@Database(entities = { User.class, Token.class }, version = 1)
+@Database(entities = { User.class, Token.class, Project.class, Branch.class,
+    Commit.class }, version = 1)
 @TypeConverters(Token.TokenTypeConverter.class)
 public abstract class AppDatabase extends RoomDatabase {
   public abstract UserRepository userRepository();
